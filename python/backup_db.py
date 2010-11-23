95eeeba1b6d8eafd6798805cc1dc52411b2e1dcc
Eric Lo <lxneng@gmail.com>
2010-11-23 23:25:51 +0800

    hahah

--- python/backup_db.py
+++ python/backup_db.py
@@ -1,41 +1,43 @@
-#!/usr/bin/python 
+#!/usr/bin/python
+# encoding: utf-8
+ 
 import os 
 import time 
 import datetime
 import MySQLdb
 
 #config vars 
-dbuser="root"
-dbpwd=""
-dbhost="localhost"
-workdir="/var/backups"
+dbuser = "root"
+dbpwd = ""
+dbhost = "localhost"
+workdir = "/var/backups"
 socket = ""
 today = (datetime.datetime.today()).strftime("%Y%m%d")
 yesterday = (datetime.datetime.today() - datetime.timedelta(1)).strftime("%Y%m%d")
 the_day_before_yesterday = (datetime.datetime.today() - datetime.timedelta(2)).strftime("%Y%m%d")
 def dumpdb(dbname): 
-    sqlvalformat='''mysqldump --single-transaction %s >%s'''
-    tarvalformat='''tar --directory=%s -zcvf %s %s'''
-    dumpfile=os.path.join(workdir,dbname+today+".sql")
-    if os.path.isfile((os.path.join(workdir,dbname+the_day_before_yesterday+".tar.gz"))):
-    	os.remove(os.path.join(workdir,dbname+the_day_before_yesterday+".tar.gz"))# delete the day before yesterday mysql backup file 
-    zipfile=os.path.join(workdir,dbname+today+".tar.gz")
-    sqlval=sqlvalformat % (dbname,dumpfile) 
-    result=os.system(sqlval) 
-    tarval=tarvalformat % (workdir,zipfile,dbname+today+".sql") 
-    result=os.system(tarval) 
+    sqlvalformat = '''mysqldump --single-transaction %s >%s'''
+    tarvalformat = '''tar --directory=%s -zcvf %s %s'''
+    dumpfile = os.path.join(workdir, dbname + today + ".sql")
+    if os.path.isfile((os.path.join(workdir, dbname + the_day_before_yesterday + ".tar.gz"))):
+    	os.remove(os.path.join(workdir, dbname + the_day_before_yesterday + ".tar.gz"))# delete the day before yesterday mysql backup file 
+    zipfile = os.path.join(workdir, dbname + today + ".tar.gz")
+    sqlval = sqlvalformat % (dbname, dumpfile) 
+    result = os.system(sqlval) 
+    tarval = tarvalformat % (workdir, zipfile, dbname + today + ".sql") 
+    result = os.system(tarval) 
     os.remove(dumpfile)
 def dumpdb_all(dbname='all_databases'): 
-    sqlvalformat='''mysqldump --single-transaction --all-databases >%s'''
-    tarvalformat='''tar --directory=%s -zcvf %s %s'''
-    dumpfile=os.path.join(workdir,dbname+today+".sql")
-    if os.path.isfile((os.path.join(workdir,dbname+the_day_before_yesterday+".tar.gz"))):
-    	os.remove(os.path.join(workdir,dbname+the_day_before_yesterday+".tar.gz"))# delete the day before yesterday mysql backup file 
-    zipfile=os.path.join(workdir,dbname+today+".tar.gz")
-    sqlval=sqlvalformat % (dumpfile) 
-    result=os.system(sqlval) 
-    tarval=tarvalformat % (workdir,zipfile,dbname+today+".sql") 
-    result=os.system(tarval) 
+    sqlvalformat = '''mysqldump --single-transaction --all-databases >%s'''
+    tarvalformat = '''tar --directory=%s -zcvf %s %s'''
+    dumpfile = os.path.join(workdir, dbname + today + ".sql")
+    if os.path.isfile((os.path.join(workdir, dbname + the_day_before_yesterday + ".tar.gz"))):
+    	os.remove(os.path.join(workdir,dbname + the_day_before_yesterday + ".tar.gz"))# delete the day before yesterday mysql backup file 
+    zipfile = os.path.join(workdir, dbname + today + ".tar.gz")
+    sqlval = sqlvalformat % (dumpfile) 
+    result = os.system(sqlval) 
+    tarval = tarvalformat % (workdir, zipfile, dbname + today + ".sql") 
+    result = os.system(tarval) 
     os.remove(dumpfile)
 
 def main(): 
